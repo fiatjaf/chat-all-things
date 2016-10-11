2c23b66128b27a7c92ee81c8495c1862c0110760
fiatjaf <fiatjaf@gmail.com>
2016-10-11 16:00:59 -0400

    focusing on cards.

--- Main.elm
+++ Main.elm
@@ -15,16 +15,6 @@ import String
 import Task
 import Debug exposing (log)
 
-main =
-    Navigation.program
-        (Navigation.makeParser (\l -> l))
-        { init = init
-        , view = view
-        , update = update
-        , urlUpdate = \msg model -> model ! []
-        , subscriptions = subscriptions
-        }
-
 
 -- MODEL
 
@@ -32,7 +22,7 @@ init : Location -> (Model, Cmd Msg)
 init _ =
     ( Model
         "fiatjaf"
-        [] [] ""
+        [] "" [] None
         ( Dict.fromList
             [ ("fiatjaf", "https://secure.gravatar.com/avatar/b760f503c84d1bf47322f401066c753f.jpg?s=140")
             ]
@@ -42,34 +32,39 @@ init _ =
 
 type alias Model =
     { me : String
-    , cards : List Card
     , messages : List Message
     , typing : String
+    , cards : List Card
+    , selectedCard : SelectStatus
     , userPictures : Dict String String
     }
 
 type alias Card =
-    { name : String
+    { id : String
+    , name : String
     , desc : String
     , comments : List Message
     }
 
 type alias Message =
-    { author : String
+    { id : String
+    , author : String
     , text : String
     }
 
+type SelectStatus = None | Loading String | Focused Card
 
 -- UPDATE
 
 type Msg
     = TypeMessage String
-    | PostMessage
-    | AddMessage Message | AddCard Card
+    | PostMessage | ClickCard String
+    | AddMessage Message | AddCard Card | FocusCard Card
     | NoOp String
 
 port pouchPut : Value -> Cmd msg
 port pouchCreate : Value -> Cmd msg
+port loadCard : String -> Cmd msg
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
@@ -96,6 +91,26 @@ update msg model =
                     else Cmd.none
             in
                 { model | typing = "" } ! [ newmessage, newcard ]
+        ClickCard id ->
+            if id == "" then
+                { model | selectedCard = None } ! []
+            else
+                case model.selectedCard of
+                    None -> { model | selectedCard = Loading id } ! [ loadCard id ]
+                    Loading loadingId ->
+                        if loadingId == id then (model, Cmd.none)
+                        else { model | selectedCard = Loading id } ! [ loadCard id ]
+                    Focused focused ->
+                        if focused.id == id then (model, Cmd.none)
+                        else { model | selectedCard = Loading id } ! [ loadCard id ]
+        FocusCard card ->
+            ( case model.selectedCard of
+                None -> { model | selectedCard = Focused card }
+                Loading loadingId ->
+                    if loadingId /= card.id then model
+                    else { model | selectedCard = Focused card }
+                Focused _ -> { model | selectedCard = Focused card }
+            ) ! []
         AddMessage message ->
             { model | messages = message :: model.messages } ! []
         AddCard card ->
@@ -107,26 +122,30 @@ update msg model =
 
 port pouchMessages : (Value -> msg) -> Sub msg
 port pouchCards : (Value -> msg) -> Sub msg
+port cardLoaded : (Value -> msg) -> Sub msg
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     let
-        messageDecoder = JD.object2 Message ("author" := JD.string) ("text" := JD.string)
-        cardDecoder = JD.object3 Card ("name" := JD.string) ("desc" := JD.string) (JD.succeed [])
+        messageDecoder = JD.object3 Message
+            ("_id" := JD.string)
+            ("author" := JD.string)
+            ("text" := JD.string)
+        cardDecoder = JD.object4 Card
+            ("_id" := JD.string)
+            ("name" := JD.string)
+            ("desc" := JD.string)
+            (JD.succeed [])
+        decodeOrFail : JD.Decoder a -> (a -> Msg) -> Value -> Msg
+        decodeOrFail decoder tagger value =
+            case decodeValue decoder value of
+                Ok decoded -> tagger decoded
+                Err err -> NoOp <| log ("error decoding " ++ (toString value)) err
     in
         Sub.batch
-            [ pouchMessages
-                ( \v ->
-                    case decodeValue messageDecoder (log "m" v) of
-                        Ok message -> AddMessage message
-                        Err err -> NoOp <| log "error decoding Message" err
-                )
-            , pouchCards
-                ( \v ->
-                    case decodeValue cardDecoder v of
-                        Ok card -> AddCard card
-                        Err err -> NoOp <| log "error decoding Card" err
-                )
+            [ pouchMessages <| decodeOrFail messageDecoder AddMessage
+            , pouchCards <| decodeOrFail cardDecoder AddCard
+            , cardLoaded <| decodeOrFail cardDecoder FocusCard
             ]
 
 -- VIEW
@@ -160,22 +179,51 @@ messageView pictures message =
             Nothing -> "https://api.adorable.io/avatars/140/" ++ message.author ++ ".png"
             Just url -> url
     in
-        div [ class "message" ]
-            [ div [ class "author" ]
-                [ img [ src authorURL ] []
-                , text message.author
+        div [ class "message", id message.id ]
+            [ img [ src authorURL ] []
+            , div []
+                [ strong [] [ text message.author ]
+                , div [ class "text" ] [ text message.text ]
                 ]
-            , div [ class "text" ] [ text message.text ]
             ]
 
 cardsView : Model -> Html Msg
 cardsView model =
-    div []
-        (List.map (lazy cardView) model.cards)
+  case model.selectedCard of
+    Focused card -> div [ id "fullcard" ] [ lazy fullCardView card ]
+    _ ->
+        div [ id "cardlist" ]
+            (model.cards
+                |> List.take 10
+                |> List.map (lazy briefCardView)
+            )
 
-cardView : Card -> Html Msg
-cardView card =
-    div [ class "card" ]
-        [ b [] [ text card.name ]
+briefCardView : Card -> Html Msg
+briefCardView card =
+    div [ class "card", id card.id ]
+        [ b [ onClick <| ClickCard card.id ] [ text card.name ]
+        , ( if card.desc == "" then text ""
+            else p [] [ text (card.desc |> String.left 60 |> (++) "...") ]
+          )
+        ]
+
+fullCardView : Card -> Html Msg
+fullCardView card =
+    div [ class "card", id card.id ]
+        [ div []
+            [ b [] [ text card.name ]
+            , div [ class "close", onClick <| ClickCard "" ] [ text "x" ]
+            ]
         , p [] [ text card.desc ]
         ]
+
+
+main =
+    Navigation.program
+        (Navigation.makeParser (\l -> l))
+        { init = init
+        , view = view
+        , update = update
+        , urlUpdate = \msg model -> model ! []
+        , subscriptions = subscriptions
+        }
